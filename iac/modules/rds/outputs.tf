1f8960977a81a23c6502bd410e9c8ceff6b2f734
NorD <nordazavr@gmail.com>
2022-04-23 15:41:14 +0300

    outputs fix

--- iac/modules/rds/outputs.tf
+++ iac/modules/rds/outputs.tf
@@ -1,10 +1,10 @@
 # RDS Endpoint
 output "db_prod" {
-  value       = aws_db_instance.mysql_prod.endpoint
+  value       = aws_db_instance.mysql_prod.address
   description = "DB endpoint"
 }
 
 output "db_dev" {
-  value       = aws_db_instance.mysql_dev.endpoint
+  value       = aws_db_instance.mysql_dev.address
   description = "DB endpoint"
 }
